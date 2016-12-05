61819aeeb5a84627545d3780d873c63a4a2effbb
DaveEveritt <deveritt@innotts.co.uk>
2016-12-05 22:47:07 +0000

    initial commit

---
+++ data/data.js
@@ -0,0 +1,28 @@
+let jsonString = `
+{ "lecturers": [
+  { 
+    "firstName":"Dave" ,
+    "lastName":"Everitt"
+  },
+  {
+    "firstName":"Fania",
+    "lastName":"Raczinski"
+  },
+  {
+    "firstName":"Luke",
+    "lastName":"Attwood"
+  },
+  {
+    "firstName":"Jenny",
+    "lastName":"Carter"
+  },
+  {
+    "firstName":"David",
+    "lastName":"Smallwood"
+  },
+  {
+    "firstName":"Clinton",
+    "lastName":"Ingrams"
+  }
+]}
+`;
