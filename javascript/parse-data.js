75a0fbc8d332c7c09fcf3f26178a2cd3c7602c8d
Dave Everitt <deveri00@LEC-ADMIN.dmu.ac.uk>
2017-11-21 14:22:51 +0000

    solution

--- javascript/parse-data.js
+++ javascript/parse-data.js
@@ -9,8 +9,11 @@
     Rewrite this to make it *loop* through all the lecturers' names. 
     (Note the ES6 'template literal' syntax):
   */
-  let theData =  `${obj.lecturers[1].firstName} ${obj.lecturers[1].lastName}`;
+  let theData = "";
   
+  for(key in obj.lecturers) {
+    theData +=`<li>${obj.lecturers[key].firstName} ${obj.lecturers[key].lastName}</li>`;
+  }
   // populates the chosen element's content with the data:
   demoData.innerHTML = theData; 
 
