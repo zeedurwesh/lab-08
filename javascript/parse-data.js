5085200b9346e444abde5ed10c7a9e56ba859e8f
Dave Everitt <deveri00@LEC-ADMIN.dmu.ac.uk>
2017-11-22 09:13:08 +0000

    added more comments

--- javascript/parse-data.js
+++ javascript/parse-data.js
@@ -11,7 +11,12 @@
 	Clue: use "for key in..." and try to work out the syntax
     (Note the ES6 'template literal' syntax - you can add HTML tags)
   */
+  
+  // set theData here to an empty string
+  
+  // create a loop around this line with "for ... in" (look it up)_
   let theData = `${obj.lecturers[1].firstName} ${obj.lecturers[1].lastName}`;
+  // end the for loop here
   
   // populates the chosen element's content with the data:
   demoData.innerHTML = theData; 
